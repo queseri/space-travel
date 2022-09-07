dc7813881b63eea6f40cb1317c514b8732e62e58
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-09-07 05:31:07 +0200

    fix: mobile - stop scroll when main menu is open, allow scroll from tablets onwards

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuControl = () => {
     const closeIcon = document.querySelector(".close")
     const hamburger = document.querySelector(".hamburger")
     const navMenu = document.querySelector(".navbar-collapse")
+    const body = document.querySelector("body")
     console.log("button clicked")
     menuBtn.classList.toggle("show-menu-js")
     closeIcon.classList.toggle("hide-icon-js")
@@ -11,9 +12,11 @@ const menuControl = () => {
     if (menuBtn.classList.contains("show-menu-js")) {
         menuBtn.setAttribute("aria-expanded", true)
         navMenu.classList.add("show-menu-js")
+        body.classList.add("body-js")
     } else {
         menuBtn.setAttribute("aria-expanded", false)
         navMenu.classList.remove("show-menu-js")
+        body.classList.remove("body-js")
     }
 }
 
